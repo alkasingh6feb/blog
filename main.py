0ecb2730d988b266041c94534284269301a9ce41
Alka Singh <alkasingh6feb@gmail.com>
2026-06-16 14:48:37 +0530

    response model

--- main.py
+++ main.py
@@ -1,43 +1,26 @@
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
-todos = []
-class Todo(BaseModel):
+class User(BaseModel):
+    id: int
+    name: str
+    email:str
+    password:str
+
+class UserResponse(BaseModel):
     id:int
-    title:str
-    completed:bool
+    name:str
+    email:str
 
-@app.post("/todos")
-def create_todo(todo:Todo):
-    todos.append(todo)
-    return {"message":"Todo added successfully", "data":todo}
+@app.get("/user", response_model=UserResponse)
+def get_user():
+    return {
+        "id":1,
+        "name":"alka",
+        "email":"alka@gmail.com"
+        
+    }
+  
 
-@app.get("/todos")
-def get_todos():
-    return todos
-
-@app.get("/todos/{todo_id}")
-def get_todo(todo_id:int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return todo
-    return {"message":"todo not found"}
-    
-@app.put("/todos{todo_id}")
-def update_todo(todo_id:int,update_todo:Todo):
-    for index,todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos[index] = update_todo
-            return {"message":"todo updated successfully", "data":update_todo}
-    return {"message":"todo not found"}
-    
-@app.delete("/todos{todo_id}")
-def delete_todo(todo_id:int):
-    for index,todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(index)
-            return {"message":"todo deleted successfully"}
-    return {"message":"todo not found"}
