8ab2490be36c8b46a8b4677ed1e41e556bd5096e
Alka Singh <alkasingh6feb@gmail.com>
2026-06-23 19:04:31 +0530

    httpexception-custom-global-handleing

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 # pyrefly: ignore [missing-import]
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+app = FastAPI()
 
-app =FastAPI()
+#custom exception
+class UserNotFoundExeption(Exception):
+    def __init__(self,name:str):
+        self.name = name
 
-@app.get("/users")
-def get_users():
-    return{
-        "name":"alka",
-        "age": 24,
-        "data":"data fetched"
+#global exception handler
+@app.exception_handler(UserNotFoundExeption)
+def user_not_found_exception_handler(request: Request, exc: UserNotFoundExeption):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": f"User {exc.name} not found"},
+    )
+
+@app.get("/user/{name}")
+def get_user(name:str):
+    if name != "alka":
+        raise UserNotFoundExeption(name)
+    return {
+        "name": name,
     }
 
-@app.get("/users/{user_id}")
-def get_singal_user(user_id:int):
-    # pyrefly: ignore [parse-error]
-    if user_id !=1:
-        
+@app.get("/users/{user_id}")    
+def get_users(user_id: int):
+    if user_id != 1:
         raise HTTPException(
             status_code=404,
-            detail="user not found" 
+            detail="user not found"
         )
-
-    return{
-        "name":"alka",
-        "age": 24,
-        "data":"data fetched"
+    return {
+        "id": 1,
+        "name": "alka"
     }
