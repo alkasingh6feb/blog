877cc57a01aec6c0d2d0c5d103296064e1550a70
Alka Singh <alkasingh6feb@gmail.com>
2026-06-24 18:45:04 +0530

    Depends

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-# pyrefly: ignore [missing-import]
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Depends, Header, HTTPException
+
 app = FastAPI()
 
-#custom exception
-class UserNotFoundExeption(Exception):
-    def __init__(self,name:str):
-        self.name = name
+def verify_token(token: str = Header(None)):
+    if token == "mohit":
+        return True
+    else:
+        raise HTTPException(status_code=401, detail="Unauthorized")
 
-#global exception handler
-@app.exception_handler(UserNotFoundExeption)
-def user_not_found_exception_handler(request: Request, exc: UserNotFoundExeption):
-    return JSONResponse(
-        status_code=404,
-        content={"detail": f"User {exc.name} not found"},
-    )
-
-@app.get("/user/{name}")
-def get_user(name:str):
-    if name != "alka":
-        raise UserNotFoundExeption(name)
-    return {
-        "name": name,
+@app.get("/sequre-data")
+def sequre_data(user = Depends(verify_token)):
+    return{
+        "msg" : "sequre data accesssed"
     }
 
-@app.get("/users/{user_id}")    
-def get_users(user_id: int):
-    if user_id != 1:
-        raise HTTPException(
-            status_code=404,
-            detail="user not found"
-        )
-    return {
-        "id": 1,
-        "name": "alka"
-    }
+# def common_logic():
+#     return {
+#         "this is common logic"
+#     }
+
+# @app.get("/home")
+# def home(data=Depends(common_logic)):
+#     return data
+
+# @app.get("/profile")
+# def profile(data=Depends(common_logic)):
+#     return data
